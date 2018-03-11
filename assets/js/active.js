a273dcbba22b65a6ca42f35ef269529913a60af0
abnormalepic <turjohasan08@gmail.com>
2018-03-12 05:07:21 +0600

    Add files via upload

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,4 +1,4 @@
-// Instantiate the Bootstrap carousel
+// Instantiate the Bootstrap carousel 3ITEM
 $('.multi-item-carousel').carousel({
   interval: false
 });
@@ -18,3 +18,31 @@ $('.multi-item-carousel .item').each(function(){
   	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
   }
 });
+// Instantiate the Bootstrap carousel 2ITEM
+$('.multi-item-carousel-2').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel-2 .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+});
+var totalSteps = 4;
+
+$('#myCarousel1').carousel();
+$('#myCarousel1').on('slide.bs.carousel', function (e) {
+  
+  //update progress
+  var step = $(e.relatedTarget).data('step');
+  var percent = (parseInt(step) / totalSteps) * 100;
+  
+  $('.progress-bar').css({width: percent + '%'});
+  $('.progress-bar').text(step + "/4");
+  
+})
