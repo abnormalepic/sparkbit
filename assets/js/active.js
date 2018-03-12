64c8464f6b3979d340db6b8c93772340b6cf6872
abnormalepic <turjohasan08@gmail.com>
2018-03-12 23:42:51 +0600

    Add files via upload

--- assets/js/active.js
+++ assets/js/active.js
@@ -77,3 +77,17 @@ $(".navbar a").on('click', function(event) {
   } // End if
 
 });
+
+
+/***************carousel swipe ***********/
+$(".carousel").swipe({
+
+  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+    if (direction == 'left') $(this).carousel('next');
+    if (direction == 'right') $(this).carousel('prev');
+
+  },
+  allowPageScroll:"vertical"
+
+});
